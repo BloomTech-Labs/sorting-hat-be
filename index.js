c55f9e34c2d125b72829b932194943abe6bd3677
aaronjan <aaronjan98@gmail.com>
2020-03-19 08:10:58 -0700

    index.js and server.js updated to listen on port 3333
    Co-Authored-By: Tomas Ortiz <tomasortiz@protonmail.com>
    Co-Authored-By: Jacob Prudent <prudentj@users.noreply.github.com>
    Co-Authored-By: Arin Ramer <arinramer@gmail.com>
    Co-Authored-By: Braden <1sdc0d3r@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config({ path: './config/.env'});
+require('colors');
+
+const server = require('./routes/api/server.js');
+
+const PORT = process.env.PORT || 3333;
+
+server.listen(PORT, () => console.log(`
+    \n*** Server running in ${process.env.NODE_ENV} mode on port ${PORT} ***\n
+`.blue.bold));
