46ea0359880ac928268f915f48be6e2186470fb1
aaronjan <aaronjan98@gmail.com>
2020-03-19 16:08:52 -0700

    Create 20200319155136_tracks.js
    Co-Authored-By: Tomas Ortiz <tomasortiz@protonmail.com>
    Co-Authored-By: Jacob Prudent <prudentj@users.noreply.github.com>
    Co-Authored-By: Arin Ramer <arinramer@gmail.com>
    Co-Authored-By: Braden <1sdc0d3r@users.noreply.github.com>

---
+++ database/migrations/20200319155136_tracks.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('tracks', tbl => {
+        tbl.increments();
+          
+        tbl.decimal('fullstack', 3, 2)
+            .defaultTo(0);
+  
+        tbl.decimal('iOS', 3, 2)
+            .defaultTo(0);
+  
+        tbl.decimal('andriod', 3, 2)
+            .defaultTo(0);
+  
+        tbl.decimal('UX', 3, 2)
+            .defaultTo(0);
+  
+        tbl.decimal('DS', 3, 2)
+            .defaultTo(0);
+
+      // Foreign Keys
+        tbl.integer('answer_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('answers')
+          .onUpdate('CASCADE')
+          .onDelete('RESTRICT');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('tracks');
+};
