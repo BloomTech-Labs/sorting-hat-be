5d338ee848c8c8b7fb5c5afd0ec803cb77d7893c
aaronjan <aaronjan98@gmail.com>
2020-03-19 17:18:26 -0700

    Create 01-questions.js
    Co-Authored-By: Tomas Ortiz <tomasortiz@protonmail.com>
    Co-Authored-By: Jacob Prudent <prudentj@users.noreply.github.com>
    Co-Authored-By: Arin Ramer <arinramer@gmail.com>
    Co-Authored-By: Braden <1sdc0d3r@users.noreply.github.com>

---
+++ database/seeds/01-questions.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('questions').del()
+    .then(function () {
+      // Inserts seed entries
+      const questions = [
+        { question: 'Ultrices neque ullamcorper, arcu luctus, sapien congue amet elit nibh?' },
+        { question: 'Lorem ipsum dolor sit amet, wisi eget dolor montes nulla odio, nonummy est parturient quis parturient amet?' },
+        { question: 'Consectetuer penatibus sit sit risus pharetra et?' },
+        { question: 'At porta eros turpis mauris varius iaculis, eros eu nibh lorem et?' },
+        { question: 'varius risus sollicitudin aliquet ut nibh quis sollicitudin ut nulla?' }
+      ];
+
+      return knex('questions').insert(questions);
+    });
+};
